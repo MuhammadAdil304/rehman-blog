44ad1017af7b887fab45d14c63f142bba6407f5c
MuhammadAdil304 <hafizmuhammadadil6@gmail.com>
2024-04-18 20:49:24 +0500

    Header Component is Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Header from '../components/Header'
 
-export default function App() {
+export default function AppRouter() {
   return (
-    <div className='text-3xl text-red-500'>App</div>
+    <div>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
