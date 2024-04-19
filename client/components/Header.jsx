91e7dc51899cfa0b28e56dd173bb48ce637e875a
MuhammadAdil304 <hafizmuhammadadil6@gmail.com>
2024-04-19 14:24:33 +0500

    Create Backend Server

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -8,8 +8,8 @@ export default function Header() {
   const path = useLocation().pathname;
   return (
     <Navbar className='border-b-2'>
-      <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-blue-500 via-blue-400 to-blue-500 text-white rounded-lg'>Rehman's</span>
+      <Link to='/' className='self-center whitespace-nowrap  md:text-xl font-semibold dark:text-white'>
+        <span className='px-2 py-1 bg-gradient-to-r from-blue-500 via-sky-300 to-blue-500 text-white rounded-lg'>Rehman's</span>
         Blog
       </Link>
       <form>
@@ -20,15 +20,15 @@ export default function Header() {
           className='hidden md:block'
         />
       </form>
-      <Button className='w-12 h-10 md:hidden' color='gray' pill>
+      <Button className='w-12 h-10 md:hidden' color='gray' >
         <AiOutlineSearch />
       </Button>
        <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden md:block' color='gray' pill>
+        <Button className='w-12 h-10 hidden md:block' color='gray' >
           <FaMoon/>
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>
+          <Button  className='bg-gradient-to-r  from-blue-500 via-sky-300 to-blue-500 hover:from-sky-500 hover:via-blue-300 hover:to-sky-500' outline>
             Sign In
           </Button>
         </Link>
