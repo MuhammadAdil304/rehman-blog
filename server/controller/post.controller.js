9f28defeab4f07aa47d467801e881b3356374e37
MuhammadAdil304 <hafizmuhammadadil6@gmail.com>
2024-05-05 20:01:50 +0500

    Work Updated

--- server/controller/post.controller.js
+++ server/controller/post.controller.js
@@ -22,6 +22,51 @@ const PostController = {
         catch (error) {
             res.status(500).send(SendResponse(false, error.message, null))
         }
+    },
+    getPosts: async (req, res) => {
+        try {
+            const startIndex = parseInt(req.query.startIndex) || 0
+            const limit = parseInt(req.query.limit) || 9
+            const sortDirection = req.query.order === 'asc' ? 1 : -1
+            const conditions = [];
+
+            if (req.query.userId) conditions.push({ userId: req.query.userId });
+            if (req.query.category) conditions.push({ category: req.query.category });
+            if (req.query.slug) conditions.push({ slug: req.query.slug });
+            if (req.query.postId) conditions.push({ _id: req.query.postId });
+            if (req.query.searchTerm) {
+                conditions.push({
+                    $or: [
+                        { title: { $regex: req.query.searchTerm, $options: 'i' } },
+                        { content: { $regex: req.query.searchTerm, $options: 'i' } }
+                    ]
+                });
+            }
+
+            const posts = await Post.find(
+                conditions.length > 0 ? { $and: conditions } : {}
+            ).sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit);
+
+            const totalPosts = await Post.countDocuments()
+
+            const now = new Date()
+
+            const oneMonthAgo = new Date(
+                now.getFullYear(),
+                now.getMonth() - 1,
+                now.getDate(),
+            )
+
+            const lastMonthPosts = await Post.countDocuments({
+                updatedAt: {
+                    $gte: oneMonthAgo
+                }
+            })
+            res.status(200).send(SendResponse(true, null, { posts, totalPosts, lastMonthPosts }))
+        }
+        catch (error) {
+            res.status(500).send(SendResponse(false, error.message, null))
+        }
     }
 }
 
--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -4,5 +4,6 @@ const PostController = require('../controller/post.controller');
 const router = express.Router();
 
 router.post('/createPost/:id', PostController.createPost)
+router.get('/getPosts' , PostController.getPosts)
 
 module.exports = router;
